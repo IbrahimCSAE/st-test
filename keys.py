3f6cdf14bcd3a8478ea121eadd23fb5aee985f51
bbycyan <119754414+bbycyan@users.noreply.github.com>
2023-04-06 03:27:31 +0300

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  5 01:10:08 2023
+
+@author: Marwan
+"""
+
+import pickle
+from pathlib import Path
+
+import streamlit_authenticator as stauth
+
+names = ["Marwan"]
+
+usernames = ['marwan']
+passwords = ["XXX"]
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("wb") as file:
+    pickle.dump(hashed_passwords, file)
